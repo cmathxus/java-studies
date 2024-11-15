579dc6c054f54d0a2a1a48dd920a4d2058eee99c
cmathxus <kirodnv@gmail.com>
2024-11-15 18:53:12 -0300

    Checkpoint

--- src/academy/devdojo/maratonajava/javacore/Eblocosinicializacao/domain/Anime.java
+++ src/academy/devdojo/maratonajava/javacore/Eblocosinicializacao/domain/Anime.java
@@ -3,12 +3,14 @@ package academy.devdojo.maratonajava.javacore.Eblocosinicializacao.domain;
 public class Anime {
     private String nome;
     private int[] episodios;
-
-    public Anime() {
+    {
+        System.out.println("Bloco de inicialização");
         episodios = new int[100];
         for (int i = 0; i < episodios.length; i++) {
             episodios[i] = i+1;
         }
+    }
+    public Anime() {
         for(int episodio: this.episodios){
             System.out.print(episodio + " ");
         }
