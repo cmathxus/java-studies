5e5de3bdcc07195d503330dda543f9e800431105
cmathxus <kirodnv@gmail.com>
2024-11-14 23:58:01 -0300

    Checkpoint

--- src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -6,7 +6,7 @@ public class AnimeTest01 {
     public static void main(String[] args) {
         Anime anime = new Anime("Akudama Drive", "TV", 12, "Ação");
 //        anime.init("Akudama Drive", "TV", 12);
-        anime.init("Akudama Drive", "TV", 12, "Ação");
+//        anime.init("Akudama Drive", "TV", 12, "Ação");
 //        anime.setNome("Akudama Drive");
 //        anime.setTipo("TV");
 //        anime.setEpisodios(12);
