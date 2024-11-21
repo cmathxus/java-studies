6c5efeeb9687c752c062032c6369d66c317cc47a
cmathxus <kirodnv@gmail.com>
2024-11-21 15:03:22 -0300

    Java Rocketseat ShortenerUrl

---
+++ java-rocketseat-urlshortener/RedirectUrlShortener/src/main/java/com/rockseat/createUrlShortner/Main.java
@@ -0,0 +1,7 @@
+package RedirectUrlShortener.src.main.java.com.rockseat.createUrlShortner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
