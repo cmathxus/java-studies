f2b0f84691e05cfeb39a9f83e138a9098c9f34af
cmathxus <kirodnv@gmail.com>
2024-11-27 21:56:15 -0300

    Checkpoint

--- java-devdojo/src/academy/devdojo/maratonajava/javacore/Gassociacao/test/JogadorTest02.java
+++ java-devdojo/src/academy/devdojo/maratonajava/javacore/Gassociacao/test/JogadorTest02.java
@@ -6,8 +6,10 @@ import devdojo.maratonajava.javacore.Gassociacao.dominio.Time;
 public class JogadorTest02 {
     public static void main(String[] args) {
         Jogador jogador1 = new Jogador("Pelé");
+        Jogador jogador2 = new Jogador("Cassio");
         Time time = new Time("Seleção Brasileira");
         jogador1.setTime(time);
         jogador1.imprime();
+        jogador2.imprime();
     }
 }
