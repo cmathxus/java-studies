42623db777182c8c5b733dbcac0f749bf9ae5d9d
cmathxus <kirodnv@gmail.com>
2024-12-10 23:07:21 -0300

    Checkpoint

--- java-devdojo/src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
+++ java-devdojo/src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
@@ -3,9 +3,18 @@ package devdojo.maratonajava.javacore.Hheranca.dominio;
 public class Funcionario extends Pessoa{
 
     private double salario;
-
+    static {
+        System.out.println("Dentro do bloco de inicialização estático de funcionário");
+    }
+    {
+        System.out.println("Dentro do bloco de inicialização de funcionário 1");
+    }
+    {
+        System.out.println("Dentro do bloco de inicialização de funcionário 2");
+    }
     public Funcionario(String nome){
         super(nome);
+        System.out.println("Dentro do construtor de funcionário");
     }
 
     public void imprime(){
